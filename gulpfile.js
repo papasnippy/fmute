5767305daadf9bbbc618f43292671846d8f8e2b2
papasnippy <papasnippy@gmail.com>
2017-04-07 21:24:55 -0400

    Removed umd webpack build.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,38 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const tslint = require('gulp-tslint');
 const tsp = ts.createProject('tsconfig.json', { declaration: true });
+const SRC = [
+    './src/**/*.ts',
+    './src/**/*.tsx'
+];
 
-gulp.task('package.json', () => gulp.src('./package.json').pipe(gulp.dest('./dist')));
+gulp.task('package.json', () => {
+    return gulp
+        .src([
+            './package.json',
+            './README.md'
+        ])
+        .pipe(gulp.dest('./dist'));
+});
 
-gulp.task('build', () => gulp.src('./src/**/*.ts').pipe(tsp()).pipe(gulp.dest('./dist')));
+gulp.task('tslint', () => {
+    return gulp
+        .src(SRC)
+        .pipe(tslint({
+            formatter: 'verbose'
+        }))
+        .pipe(tslint.report({
+            emitError: false,
+            summarizeFailureOutput: 1
+        }));
+});
 
-gulp.task('default', ['package.json', 'build']);
+gulp.task('build', ['tslint'], () => {
+    return gulp
+        .src(SRC)
+        .pipe(tsp())
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('default', ['package.json', 'tslint', 'build']);
